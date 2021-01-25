bcd33a5b8e71ad7b6439328ce773a6a63d476b68
Peter Mein <peter@infratron.io>
2021-01-25 10:12:51 +0100

    fix: added pointer to conn
    chore: added toml to set production settings

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -19,7 +19,7 @@ pub fn persons(conn: DbConn) -> Result<Json<Vec<Person>>, String> {
 
     use crate::database::schema::persons::dsl::*;
 
-    persons.load::<Person>(&conn)
+    return persons.load::<Person>(&*conn)
         .map_err(|err| -> String {
         println!("Error querying page views: {:?}", err);
         "Error querying page views from the database".into()
